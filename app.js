9ae1d4609ebf0e056582f7faa3dc669dd576a6c4
Subhajit <subhajit7252@gmail.com>
2024-08-25 13:09:23 +0530

    initial server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);    
+});
